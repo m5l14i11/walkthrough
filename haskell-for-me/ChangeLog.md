ae9c39c3dd2a6d9be4bc3d50f22400aeff059a71
Siarhei Melnik <siarhei.m.a@gmail.com>
2021-10-13 23:51:53 +0300

    housekeeping

--- haskell-for-me/ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for haskell-for-me
-
-## Unreleased changes
--- haskell-for-me/src/Lib.hs
+++
@@ -1,7 +0,0 @@
-module Lib where
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
-
-addMeOne :: Int -> Int
-addMeOne x = (+) x 1
